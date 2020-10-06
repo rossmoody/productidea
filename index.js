dba384222b7d67746bf2ee66cf87319cc2ddbc19
rossmoody <fiftyfivehis@gmail.com>
2020-10-05 22:00:24 -0700

    commit message

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-console.log("I am working");
+twttr.ready(function (twttr) {
+  fetch("https://45a7f9eb-3cc0-43ec-9644-5c1f4f407873.mock.pstmn.io")
+    .then((response) => response.text())
+    .then((results) => {
+      const arr = [];
+      const obj = JSON.parse(results);
+      const data = obj.data;
 
-fetch("https://45a7f9eb-3cc0-43ec-9644-5c1f4f407873.mock.pstmn.io")
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
+      for (const entry of data) {
+        arr.push(entry);
+      }
+
+      const finals = data.filter(
+        (entry) => entry.public_metrics.like_count > 5
+      );
+
+      finals.forEach((entry) => {
+        twttr.widgets.createTweet(
+          `${entry.id}`,
+          document.getElementById("container"),
+          {
+            conversation: "none",
+            cards: "hidden",
+          }
+        );
+      });
+    })
+    .catch((error) => console.log("error", error));
+});
