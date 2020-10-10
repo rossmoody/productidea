69b5fea4185116121fba291845fbd2b2c87d9221
rossmoody <fiftyfivehis@gmail.com>
2020-10-10 12:48:20 -0700

    commit message

--- functions/hello.js
+++ functions/hello.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     type: process.env.FIRE_TYPE,
     project_id: process.env.FIRE_PROJECT_ID,
     private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
-    private_key: process.env.FIRE_PRIVATE_KEY,
+    private_key: process.env.FIRE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     client_email: process.env.FIRE_CLIENT_EMAIL,
     client_id: process.env.FIRE_CLIENT_ID,
     auth_uri: process.env.FIRE_AUTH_URI,
