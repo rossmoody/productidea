823040460bd2f7bd86ea9b07376c895533a7db9d
rossmoody <fiftyfivehis@gmail.com>
2020-10-10 20:09:36 -0700

    commit message

--- functions/hello.js
+++ functions/hello.js
@@ -1,18 +1,18 @@
-const admin = require("firebase-admin");
+// const admin = require("firebase-admin");
 // // const needle = require("needle");
 
-const creds = {
-  type: process.env.FIRE_TYPE,
-  project_id: process.env.FIRE_PROJECT_ID,
-  private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
-  private_key: process.env.FIRE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIRE_CLIENT_EMAIL,
-  client_id: process.env.FIRE_CLIENT_ID,
-  auth_uri: process.env.FIRE_AUTH_URI,
-  token_uri: process.env.FIRE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIRE_AUTH_PROVIDER,
-  client_x509_cert_url: process.env.FIRE_CLIENT_CERT,
-};
+// const creds = {
+//   type: process.env.FIRE_TYPE,
+//   project_id: process.env.FIRE_PROJECT_ID,
+//   private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
+//   private_key: process.env.FIRE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//   client_email: process.env.FIRE_CLIENT_EMAIL,
+//   client_id: process.env.FIRE_CLIENT_ID,
+//   auth_uri: process.env.FIRE_AUTH_URI,
+//   token_uri: process.env.FIRE_TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.FIRE_AUTH_PROVIDER,
+//   client_x509_cert_url: process.env.FIRE_CLIENT_CERT,
+// };
 
 // // // Twitter API creds
 // // const token = process.env.BEARER_TOKEN;
@@ -49,10 +49,10 @@ const creds = {
 // // }
 
 // Firebase
-admin.initializeApp({
-  credential: admin.credential.cert(creds),
-  databaseURL: "https://i-need-a-product-idea.firebaseio.com",
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(creds),
+//   databaseURL: "https://i-need-a-product-idea.firebaseio.com",
+// });
 
 // function getDate() {
 //   const dateObj = new Date();
@@ -64,8 +64,8 @@ admin.initializeApp({
 
 // const todaysDate = getDate();
 
-const db = admin.database();
-const ref = db.ref();
+// const db = admin.database();
+// const ref = db.ref();
 // const todayRef = db.ref(todaysDate);
 
 // ref.once("value", (snapshot) => {
