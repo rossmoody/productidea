1b25c3914b13b7c0f7caac75f31b88c11fe5abb8
rossmoody <fiftyfivehis@gmail.com>
2020-10-10 20:58:45 -0700

    commit message

--- functions/hello.js
+++ functions/hello.js
@@ -96,7 +96,7 @@ exports.handler = async (event, context, callback) => {
       return callback(null, {
         statusCode: 200,
         body: JSON.stringify({
-          data: val,
+          data: "hello there",
         }),
       });
     });
