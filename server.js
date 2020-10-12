8b504bf83fe144186f66a57410cbf4d51f3a95ef
rossmoody <fiftyfivehis@gmail.com>
2020-10-12 15:08:35 -0700

    commit message

--- server.js
+++ server.js
@@ -1,42 +1,6 @@
 const express = require("express");
-const admin = require("firebase-admin");
 
 // Server
 const app = express();
 app.listen(3000, () => console.log("Server is listening on port 3000"));
 app.use(express.static("local"));
-
-const creds = {
-  type: process.env.FIRE_TYPE,
-  project_id: process.env.FIRE_PROJECT_ID,
-  private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
-  private_key: process.env.FIRE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIRE_CLIENT_EMAIL,
-  client_id: process.env.FIRE_CLIENT_ID,
-  auth_uri: process.env.FIRE_AUTH_URI,
-  token_uri: process.env.FIRE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIRE_AUTH_PROVIDER,
-  client_x509_cert_url: process.env.FIRE_CLIENT_CERT,
-};
-
-// Firebase
-admin.initializeApp({
-  credential: admin.credential.cert(creds),
-  databaseURL: "https://i-need-a-product-idea.firebaseio.com",
-});
-
-const db = admin.database();
-const ref = db.ref();
-
-app.get("/.netlify/functions/hello", function (req, res) {
-  ref.once("value", (snapshot) => {
-    const val = snapshot.val();
-    res.send(JSON.stringify(val));
-  });
-});
-
-// Twitter stuff
-
-app.get("/.netlify/functions/tweets", function (req, res) {
-  console.log({ data: "this is working" });
-});
--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 // import { eventListeners } from "./event-listeners";
 
 (async function init() {
-  const response = await fetch(process.env.INAPI_URL);
-  const json = await response.json();
-  const netlifyJson = json.data;
-  console.log(netlifyJson);
-
-  // const massiveArrayOfTweetObjects = [];
-
-  // // Netlify function
-  // const netlifyJson = json.data;
-  // for (const dayObj in netlifyJson) {
-  //   const values = Object.values(netlifyJson[dayObj]);
-  //   values.forEach((entry) => {
-  //     massiveArrayOfTweetObjects.push(entry);
-  //   });
-  // }
-
-  // // Local function
-  // for (const dayObj in json) {
-  //   json[dayObj].forEach((entry) => {
-  //     massiveArrayOfTweetObjects.push(entry);
-  //   });
-  // }
-
-  // eventListeners(massiveArrayOfTweetObjects);
+  console.log("fuck this");
 })();
