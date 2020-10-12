4fe98ea517e79f841e1a9ce463479d2cd4c34eed
rossmoody <fiftyfivehis@gmail.com>
2020-10-11 23:16:35 -0700

    commit message

--- functions/tweets.js
+++ functions/tweets.js
@@ -56,6 +56,19 @@ async function getTweets() {
 const now = new Date(Date.now()).toISOString();
 
 // Firebase
+const creds = {
+  type: process.env.FIRE_TYPE,
+  project_id: process.env.FIRE_PROJECT_ID,
+  private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
+  private_key: process.env.FIRE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIRE_CLIENT_EMAIL,
+  client_id: process.env.FIRE_CLIENT_ID,
+  auth_uri: process.env.FIRE_AUTH_URI,
+  token_uri: process.env.FIRE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIRE_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIRE_CLIENT_CERT,
+};
+
 admin.initializeApp({
   credential: admin.credential.cert(creds),
   databaseURL: "https://i-need-a-product-idea.firebaseio.com",
