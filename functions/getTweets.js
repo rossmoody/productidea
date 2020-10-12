0151ccccb4b069d53a5f0ac48e79f2117bf8f22d
rossmoody <fiftyfivehis@gmail.com>
2020-10-11 21:04:03 -0700

    commit message

---
+++ functions/getTweets.js
@@ -0,0 +1,17 @@
+// const now = new Date(Date.now()).toISOString();
+// const yesterday = new Date(Date.now() - 864000 * 1000).toISOString();
+// const todayRef = db.ref(now);
+
+// getTweets(yesterday).then((results) => {
+//   const dayArr = [];
+
+//   results.forEach((queryArr) => {
+//     queryArr.forEach((tweet) => {
+//       dayArr.push(tweet);
+//     });
+//   });
+
+//   todayRef.set(dayArr);
+// });
+
+console.log("This is working");
