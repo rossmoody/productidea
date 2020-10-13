989c94b5a277ea199a6402700f5fab1867dae87c
rossmoody <fiftyfivehis@gmail.com>
2020-10-12 17:08:25 -0700

    commit message

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@
 
 ;(async function init() {
   const response = await fetch(process.env.INAPI_URL)
-  console.log(response)
-  console.log(response.data)
-  const json = await response.json()
+  const json = await response.data.json()
   console.log(json)
   console.log(json.data)
 })()
