74f7e1945266e09bc810c49c196b3b95e8d23ce4
rossmoody <fiftyfivehis@gmail.com>
2020-10-17 20:02:07 -0700

    commit message

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # I Need a Product Idea
 
-## 1. Run server locally with `node server.js`
+1. Improve the server automation so it isn't reliant on someone refreshing every day
+2. Improve the server automation to update entries a few times a day
+3. Improve the load more functionality to not always be showing unless there actually ARE more
+4. Set up a loader for rendering
 
-The content in server.js should be copies over to hello.js in functions folder for proper application.
+## Example object
+
+```
+{​
+    created_at: "2020-10-17T12:32:14.000Z"
+    id: "1317443300979245056"
+    public_metrics: {
+        like_count: 31
+        quote_count: 0
+        reply_count: 3
+        retweet_count: 6
+    }
+    query_id: "new-product-request"
+    text: "..."
+}
+```
---
+++ functions/tweets.js
@@ -0,0 +1,146 @@
+const needle = require("needle")
+const admin = require("firebase-admin")
+
+// Database stuff
+const creds = {
+  type: process.env.FIRE_TYPE,
+  project_id: process.env.FIRE_PROJECT_ID,
+  private_key_id: process.env.FIRE_PRIVATE_KEY_ID,
+  private_key: process.env.FIRE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIRE_CLIENT_EMAIL,
+  client_id: process.env.FIRE_CLIENT_ID,
+  auth_uri: process.env.FIRE_AUTH_URI,
+  token_uri: process.env.FIRE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIRE_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIRE_CLIENT_CERT
+}
+
+//
+//
+// Twitter API creds
+const token = process.env.BEARER_TOKEN
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent"
+
+const queries = [
+  {
+    string: `"I wish someone would make"`,
+    query_id: "i-wish-someone-would-make"
+  },
+  {
+    string: `"great app idea"`,
+    query_id: "great-app-idea"
+  },
+  {
+    string: `"an app where"`,
+    query_id: "an-app-where"
+  },
+  {
+    string: `"amazing product idea"`,
+    query_id: "amazing-product-idea"
+  },
+  {
+    string: `"new product idea"`,
+    query_id: "new-product-request"
+  }
+]
+
+const queriesTwo = [
+  {
+    string: `"does anyone know of an app"`,
+    query_id: "does-anyone-know-of-an-app"
+  },
+  {
+    string: `"i wish there was a service"`,
+    query_id: "i-wish-there-was-a-service"
+  },
+  {
+    string: `"i wish there was an app"`,
+    query_id: "i-wish-there-was-an-app"
+  },
+  {
+    string: `"wish i could find"`,
+    query_id: "wish-i-could-find"
+  },
+  {
+    string: `"#productidealist"`,
+    query_id: "productidealist"
+  },
+  {
+    string: `"#productidea"`,
+    query_id: "productidea"
+  },
+  {
+    string: `"#appidea"`,
+    query_id: "appidea"
+  }
+]
+
+async function getQuery(query) {
+  const yesterday = new Date(Date.now() - 86400 * 1000).toISOString()
+
+  const params = {
+    query: query.string,
+    "tweet.fields": "public_metrics,created_at",
+    start_time: yesterday.replace(/['"]+/g, "")
+  }
+
+  const res = await needle("get", endpointUrl, params, {
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  })
+
+  if (res.body) {
+    return res.body
+  } else {
+    throw new Error("Unsuccessful request")
+  }
+}
+
+async function getTweets(array) {
+  const init = array.map(async query => {
+    const response = await getQuery(query)
+
+    if (response.data) {
+      response.data.forEach(element => {
+        element.query_id = query.query_id
+      })
+
+      return response.data
+    }
+  })
+
+  const data = await Promise.all(init)
+
+  const dayArr = []
+
+  data.forEach(queryArr => {
+    if (queryArr) {
+      queryArr.forEach(tweet => {
+        dayArr.push(tweet)
+      })
+    }
+  })
+
+  return dayArr
+}
+
+exports.handler = async (event, context, callback) => {
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(creds),
+      databaseURL: "https://i-need-a-product-idea.firebaseio.com"
+    })
+  }
+
+  const db = admin.database()
+  const todayRef = db.ref("poop")
+
+  const tweets = await getTweets(queries)
+  const tweetsTwo = await getTweets(queriesTwo)
+  const allTweets = [...tweets, ...tweetsTwo]
+
+  todayRef.set(allTweets)
+
+  admin.app().delete()
+}
