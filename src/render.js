5426ccb0cc9d7e55a56a9ccc5b0d48a7dc5070ec
rossmoody <fiftyfivehis@gmail.com>
2020-10-30 17:09:13 -0700

    commit message

--- src/render.js
+++ src/render.js
@@ -1,24 +1,33 @@
 function makeTweets(arr, node) {
   const loader = `<svg xmlns="http://www.w3.org/2000/svg" id="loader" style="margin:auto;background:#fff" width="200" height="200" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" display="block"><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.6790123456790123s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(30 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.6172839506172838s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(60 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.5555555555555555s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(90 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.4938271604938271s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(120 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.4320987654320987s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(150 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.3703703703703703s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(180 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.3086419753086419s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(210 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.24691358024691354s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(240 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.18518518518518515s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(270 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.12345679012345677s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(300 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="-0.061728395061728385s" repeatCount="indefinite"/></rect><rect x="47.5" y="24.5" rx="2.5" ry="2.86" width="5" height="11" fill="#4c9eeb" transform="rotate(330 50 50)"><animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.7407407407407407s" begin="0s" repeatCount="indefinite"/></rect></svg>`
   node.innerHTML = loader
+  const loaderEl = document.getElementById("loader")
 
-  twttr.ready(() => {
-    arr.forEach(tweet => {
-      try {
-        twttr.widgets.createTweet(`${tweet.id}`, node, {
-          conversation: "none",
-          cards: "hidden"
-        })
-      } catch (error) {
-        console.log("Yuck, an error", error)
-      }
+  if (arr.length < 1) {
+    const div = document.createElement("div")
+    div.classList.add("no-results")
+    div.textContent = "No matching ideas to show"
+    node.appendChild(div)
+
+    loaderEl && loaderEl.remove()
+  } else {
+    twttr.ready(() => {
+      arr.forEach(tweet => {
+        try {
+          twttr.widgets.createTweet(`${tweet.id}`, node, {
+            conversation: "none",
+            cards: "hidden"
+          })
+        } catch (error) {
+          console.log("Yuck, an error", error)
+        }
+      })
     })
-  })
 
-  twttr.events.bind("rendered", () => {
-    const loader = document.getElementById("loader")
-    loader && loader.remove()
-  })
+    twttr.events.bind("rendered", () => {
+      loaderEl && loaderEl.remove()
+    })
+  }
 }
 
 export function renderTweets(tweets) {
